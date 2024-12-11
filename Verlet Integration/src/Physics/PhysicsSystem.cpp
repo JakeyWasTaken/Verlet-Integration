77c69a657e1c7419a0f0b81a6a5a75abbbbfff74
JakeyWasTaken <75340712+JakeyWasTaken@users.noreply.github.com>
2024-12-11 23:51:44 +0000

    Physics base

---
+++ Verlet Integration/src/Physics/PhysicsSystem.cpp
@@ -0,0 +1,9 @@
+#include "PhysicsSystem.h"
+
+namespace Verlet
+{
+	namespace Physics
+	{
+		
+	}
+}
---
+++ Verlet Integration/src/Physics/PhysicsSystem.h
@@ -0,0 +1,19 @@
+/*
+* // THEORY \\
+* 
+* The Physics system is entirely for simulating point masses constrainted to one another that can collide
+* IT DOES NOT interact with any meshes / models etc.
+*/
+
+#pragma once
+
+namespace Verlet
+{
+	namespace Physics
+	{
+		class PhysicsSystem
+		{
+		};
+	}
+}
+
